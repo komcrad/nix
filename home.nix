dbb804e00dc9e065c389d5a487a592880e2322b9
komcrad <konradt95@gmail.com>
2024-11-25 21:11:03 -0600

    Configure user in separate file

--- home.nix
+++ home.nix
@@ -33,8 +33,8 @@
       ]);
 
     # This needs to actually be set to your username
-    username = "<username>";
-    homeDirectory = "<home>";
+    username = (import ./user.nix).username;
+    homeDirectory = (import ./user.nix).homeDirectory;
 
     # You do not need to change this if you're reading this in the future.
     # Don't ever change this after the first build.  Don't ask questions.
