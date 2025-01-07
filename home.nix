3c444fc7088dfa7f0556a0544c6eeda94dc9009e
komcrad <konradt95@gmail.com>
2025-01-06 18:56:15 -0600

    Some nvim updates

--- home.nix
+++ home.nix
@@ -23,6 +23,10 @@
         stylua
         bazelisk
         rustup
+        gcc
+        csharp-ls
+        phpactor
+        cmake
         gnupg
         black
         awscli2
@@ -74,15 +78,14 @@
         '';
       };
       "bin/fix-docker" = {
-      executable = true;
-      text = ''
-        #!/usr/bin/env bash
-        colima stop
-        colima start
-        sudo rm /var/run/docker.sock
-        sudo ln ~/.colima/default/docker.sock /var/run
-      '';
-
+        executable = true;
+        text = ''
+          #!/usr/bin/env bash
+          colima stop
+          colima start
+          sudo rm /var/run/docker.sock
+          sudo ln ~/.colima/default/docker.sock /var/run
+        '';
       };
     };
   };
--- mac.nix
+++ mac.nix
@@ -3,6 +3,7 @@
   home = {
     packages = [
       unstable.colima
+      unstable.docker
     ];
   };
 }
